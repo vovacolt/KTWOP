bba4fb2faeb57a19075a234382cdb624ddd753ef
Глеб Мельников <glebmelnikov1999@mail.ru>
2022-03-21 21:00:04 +0300

    Add basic enemies.

---
+++ Source/EnemySystem/EnemySystem.cpp
@@ -0,0 +1,6 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EnemySystem.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EnemySystem, "EnemySystem.h" );
